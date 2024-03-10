f94dfff5314e1ad60036d666bdee526809f0f5de
k-Raiden <106054710+k-Raiden@users.noreply.github.com>
2024-03-09 21:02:53 -0500

    Add files via upload

---
+++ mgt - Copy/MortgageCal-master/Controllers/ClientController1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+using MortgageCal.Models;
+
+namespace MortgageCal.Controllers
+{
+    public class ClientController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            Mortgage Model = new Mortgage();
+            
+            return View(Model);
+
+        }
+
+        [HttpPost]
+        public IActionResult CalculateMortgage(Mortgage C )
+        {
+            C.MonthlyPayment = getMonthlyPayment(C.TotalLoanAmount,C.DownPayment,C.Apr,C.LoanTerm,C.PropertyTax,C.Insurance);
+            
+           
+
+            return View("Index",C);
+
+        }
+
+        private double getMonthlyPayment(double totalLoanAmount, double downPayment,double apr,double loanTerm,double propertyTax,double insurance)
+        {
+            //double apr = 0.056;
+           // int loanTrm = 360;
+
+            double mortgPayment1;
+            double mortgPayment2;
+            
+
+
+            mortgPayment1 = (totalLoanAmount - downPayment) * (Math.Pow((1 + apr / 12), loanTerm) * apr) / (12 * Math.Pow((1 + apr / 12), loanTerm - 1));
+
+            mortgPayment2 = (propertyTax + insurance) / 12 + mortgPayment1;
+
+            double roundMortgagePymnt = Math.Round(mortgPayment2, 2);
+
+            return roundMortgagePymnt;
+
+        }
+    }
+}
---
+++ mgt - Copy/MortgageCal-master/Models/Mortgage.cs
@@ -0,0 +1,33 @@
+﻿namespace MortgageCal.Models
+{
+    public class Mortgage
+    {
+        public double Principal { get; set; }
+        public double TotalLoanAmount { get; set; }
+
+         public double DownPayment { get; set; }
+        //Principal= loan amount.//
+        public double  PropertyTax { get; set; }
+        
+        public double Insurance { get; set; } 
+        public double Apr { get; set; }
+
+        //Apr is annual//
+
+        public double LoanTerm { get; set; }// length of the loan.
+
+        public double TotalLoanInterest{ get; set;}
+
+        public double MonthlyPayment { get; set; }
+
+
+
+
+
+
+
+
+
+
+    }
+}
